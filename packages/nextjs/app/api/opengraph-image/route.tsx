1b6316f2c26d302d404f9856d7d6865e465a3f8a
bzzzzzzzzzzzzz <99511466+bzzzzzzzzzzzzz@users.noreply.github.com>
2025-10-18 07:57:16 -0400

    added the neynar share and api folders

---
+++ packages/nextjs/app/api/opengraph-image/route.tsx
@@ -0,0 +1,28 @@
+import { ImageResponse } from "next/og";
+import { NextRequest } from "next/server";
+// import { getNeynarUser } from "~/lib/neynar";
+
+export const dynamic = 'force-dynamic';
+
+export async function GET() {
+
+    // const user = fid ? await getNeynarUser(Number(fid)) : null;
+
+  return new ImageResponse(
+    (
+      <div tw="flex h-full w-full flex-col justify-center items-center relative bg-primary">
+        {/* {user?.pfp_url && (
+          <div tw="flex w-96 h-96 rounded-full overflow-hidden mb-8 border-8 border-white">
+            <img src={user.pfp_url} alt="Profile" tw="w-full h-full object-cover" />
+          </div>
+        )} */}
+        <h1 tw="text-8xl text-white">THE RANKR APP!</h1>
+        <p tw="text-5xl mt-4 text-white opacity-80">(by Romi, Noad & Diana)</p>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 800,
+    }
+  );
+}
